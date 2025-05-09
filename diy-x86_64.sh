d6dc99ce5faf0d6a35b0e702e9e8842e7b403cdd
NERD <168670760+zhiern@users.noreply.github.com>
2025-05-10 03:16:43 +0800

    Update and rename diy-script.sh to diy-x86_64.sh

--- diy-x86_64.sh
+++ diy-x86_64.sh
@@ -387,6 +387,43 @@ git clone https://github.com/sbwml/openwrt_pkgs package/new/openwrt_pkgs
 # autocore-arm
 git clone https://github.com/sbwml/autocore-arm package/new/autocore-arm
 
+sed -i 's/O2/O2 -march=x86-64-v2/g' include/target.mk
+
+# libsodium
+sed -i 's,no-mips16 no-lto,no-mips16,g' feeds/packages/libs/libsodium/Makefile
+
+echo '#!/bin/sh
+# Put your custom commands here that should be executed once
+# the system init finished. By default this file does nothing.
+
+if ! grep "Default string" /tmp/sysinfo/model > /dev/null; then
+    echo should be fine
+else
+    echo "Generic PC" > /tmp/sysinfo/model
+fi
+
+status=$(cat /sys/devices/system/cpu/intel_pstate/status)
+
+if [ "$status" = "passive" ]; then
+    echo "active" | tee /sys/devices/system/cpu/intel_pstate/status
+fi
+
+exit 0
+'> ./package/base-files/files/etc/rc.local
+
+# 默认设置
+git clone --depth=1 -b openwrt-24.10 https://github.com/oppen321/default-settings package/new/default-settings
+
+# distfeeds.conf
+mkdir -p files/etc/opkg
+cat > files/etc/opkg/distfeeds.conf <<EOF
+src/gz openwrt_base https://mirrors.tuna.tsinghua.edu.cn/openwrt/releases/24.10.1/packages/x86_64/base
+src/gz openwrt_luci https://mirrors.tuna.tsinghua.edu.cn/openwrt/releases/24.10.1/packages/x86_64/luci
+src/gz openwrt_packages https://mirrors.tuna.tsinghua.edu.cn/openwrt/releases/24.10.1/packages/x86_64/packages
+src/gz openwrt_routing https://mirrors.tuna.tsinghua.edu.cn/openwrt/releases/24.10.1/packages/x86_64/routing
+src/gz openwrt_telephony https://mirrors.tuna.tsinghua.edu.cn/openwrt/releases/24.10.1/packages/x86_64/telephony
+EOF
+
 # install feeds
 ./scripts/feeds update -a
 ./scripts/feeds install -a
